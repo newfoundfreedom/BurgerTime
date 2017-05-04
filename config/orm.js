fc17335027fcac00bf7802630761290b6e8b9265
Joel Roberts <newfoundfreedom@gmail.com>
2017-05-04 16:47:49 -0400

    project near complete - forgot to commit - whoops

--- config/orm.js
+++ config/orm.js
@@ -1,35 +1,42 @@
-const connection = require("./connection.js");
+// Import MySQL connection.
+const connection = require('../config/connection.js');
 
-var orm = {
-//     selectAll: function(whatToSelect, tableInput) {
-//         var queryString = "SELECT ?? FROM ??";
-//         connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
-//             if (err) {
-//                 throw err;
-//             }
-//             console.log(result);
-//         });
-//     },
-//     insertOne: function(tableInput, colToSearch, valOfCol) {
-//         var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-//
-//         console.log(queryString);
-//
-//         connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
-//             console.log(result);
-//         });
-//     },
-//     updateOne: function(whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol) {
-//         var queryString = "SELECT ?? FROM ?? AS tOne";
-//         queryString = queryString + " LEFT JOIN ?? AS tTwo";
-//         queryString = queryString + " ON tOne.?? = tTwo.??";
-//
-//         console.log(queryString);
-//
-//         connection.query(queryString, [whatToSelect, tableOne, tableTwo, onTableOneCol, onTableTwoCol], function(err, result) {
-//             console.log(result);
-//         });
-//     }
+
+// Object for all our SQL statement functions.
+let orm = {
+    all: function (table, cb) {
+        let queryString = `SELECT * FROM ${table};`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(('ORM all response      = ' + JSON.stringify(result, null, 0)).inverse.red);
+            cb(result);
+        })
+    },
+    create: function (table, col, val, cb) {
+        let queryString = `INSERT INTO ${table} (${col}) VALUES ('${val}');`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(('ORM create response   = ' + JSON.stringify(result, null, 0)).inverse.red);
+            cb(result);
+        })
+    },
+    update: function (table, col, id, cb) {
+        let queryString = `UPDATE ${table} SET ${col} = true WHERE id = ${id};`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(('ORM update response   = ' + JSON.stringify(result, null, 0)).inverse.red);
+            cb(result);
+        })
+    },
+    delete: function (table, id, cb) {
+        let queryString = `DELETE FROM ${table} WHERE id = ${id};`;
+        connection.query(queryString, function (err, result) {
+            if (err) throw err;
+            console.log(('ORM delete response   = ' + JSON.stringify(result, null, 0)).inverse.red);
+            cb(result);
+        })
+    }
 };
 
+// Export the orm object for the model (cat.js).
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,30 +1,34 @@
+// dependency declarations
 const express = require("express"),
-    bodyParser = require("body-parser"),
-    methodOverride = require("method-override");
+    bodyParser = require("body-parser");
+    // methodOverride = require("method-override");
 
+// app initialization
+const PORT = process.env.PORT || 4444;
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Make Public folder accessible to app
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 // Configure Body-Parser
-// Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+// app.use(methodOverride("_method"));
+
+// Set Handlebars
 const exphbs = require("express-handlebars");
 
-// Configure Handlebars
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Require Routing file
-require('./controllers/burgertime_controller')(app);
+// Import routes and give the server access to them.
+const routes = require("./controllers/burgertime_controller");
+
+app.use("/", routes);
 
 // Start server listening
-app.listen(PORT, function() {
+app.listen(PORT, function(){
     console.log(`App listening on PORT ${PORT}`);
 });
+
